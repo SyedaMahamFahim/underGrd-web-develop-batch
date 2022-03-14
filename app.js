c019be7d29a1c9513c57aff94c913f30042d211a
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-03-14 23:12:11 +0500

    commit

---
+++ app.js
@@ -0,0 +1,52 @@
+
+let myName="maham"
+console.log("My name is ",myName,"i am fullstac")
+
+
+console.log(`My name is ${myName}.I am a full`)
+
+
+const priceSign="$"
+const allProducts=[
+    {
+        id:0,
+        title:"Men Product One",
+        price:100,
+        desc:"Lorem ipsum dolor sit ametm",
+        url:'men_1.jpg',
+        inCart:0
+    },
+    {
+        id:1,
+        title:"Men Product 2",
+        price:200,
+        desc:"Lorem ipsum dolor sit ametm",
+        url:'men_2.jpg',
+        inCart:0
+    },
+    {
+        id:3,
+        title:"Men Product 3",
+        price:300,
+        desc:"Lorem ipsum dolor sit ametm",
+        url:'men_3.jpg',
+        inCart:0
+    },
+    {
+        id:4,
+        title:"Women Product 1",
+        price:300,
+        desc:"Lorem ipsum dolor sit ametm",
+        url:'women_1.jpg',
+        inCart:0
+    },
+    {
+        id:5,
+        title:"Women Product 1",
+        price:500,
+        desc:"Lorem ipsum dolor sit ametm",
+        url:'women_2.jpg',
+        inCart:0
+    },
+
+]
---
+++ extra.js
@@ -0,0 +1,44 @@
+// let studentName=['maham','ayesha','rabia','shiza','marium','mehak']
+
+
+// // function userName(){ // function define
+
+// // } 
+// // const userName=()=> 
+// studentName.forEach((val)=>{
+//     console.log(val)
+// })
+
+
+
+// // userName("mahsm")  // Function call 
+// // Map == loop === Array
+// // Foreach == loop ===Array 
+
+
+// [{},{},{},{}]  //Array of object
+// let singleObj={
+//     id:1,
+//     title:"Product one",
+//     price:34,
+//     qty:2
+// }
+
+// console.log(singleObj.id)
+
+let products=[
+    {
+        id:1,
+        title:"Product one",
+        price:34,
+        qty:2
+    },
+    {
+        id:2,
+        title:"Product two",
+        price:6,
+        qty:5
+    },
+]
+
+console.log(products[1].id)
