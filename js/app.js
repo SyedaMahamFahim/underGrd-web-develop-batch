3d1ebe0b9c15063ebeea5d236fd7daafdf7dd65c
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-03-17 22:21:32 +0500

    commit

--- js/app.js
+++ js/app.js
@@ -1,85 +1,84 @@
-const priceSign = "$"
+const priceSign = "$";
 const allProducts = [
-    {
-        id: 0,
-        title: "Men Product 1",
-        price: 100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
-        url: 'men_1.jpg',
-        qty: 0
-    },
-    {
-        id: 1,
-        title: "Men Product 2",
-        price: 200,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
-        url: 'men_2.jpg',
-        qty: 0
-    },
-    {
-        id: 2,
-        title: "Men Product 3",
-        price: 300,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
-        url: 'men_3.jpg',
-        qty: 0
-    },
-    {
-        id: 3,
-        title: "Women Product 1",
-        price: 100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
-        url: 'women_1.jpg',
-        qty: 0
-    },
-    {
-        id: 4,
-        title: "Women Product 2",
-        price: 100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
-        url: 'women_2.jpg',
-        qty: 0
-    },
-    {
-        id: 5,
-        title: "Women Product 3",
-        price: 100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
-        url: 'women_3.jpg',
-        qty: 0
-    },
-    {
-        id: 6,
-        title: "Perfume 1",
-        price: 100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
-        url: 'perfume_1.jpg',
-        qty: 0
-    },
-    {
-        id: 7,
-        title: "Perfume 2",
-        price: 100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
-        url: 'perfume_2.jpg',
-        qty: 0
-    },
-    {
-        id: 9,
-        title: "Perfume 3",
-        price: 100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
-        url: 'perfume_3.jpg',
-        qty: 0
-    },
-
-]
+  {
+    id: 0,
+    title: "Men Product 1",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
+    url: "men_1.jpg",
+    qty: 0,
+  },
+  {
+    id: 1,
+    title: "Men Product 2",
+    price: 200,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
+    url: "men_2.jpg",
+    qty: 0,
+  },
+  {
+    id: 2,
+    title: "Men Product 3",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
+    url: "men_3.jpg",
+    qty: 0,
+  },
+  {
+    id: 3,
+    title: "Women Product 1",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
+    url: "women_1.jpg",
+    qty: 0,
+  },
+  {
+    id: 4,
+    title: "Women Product 2",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
+    url: "women_2.jpg",
+    qty: 0,
+  },
+  {
+    id: 5,
+    title: "Women Product 3",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
+    url: "women_3.jpg",
+    qty: 0,
+  },
+  {
+    id: 6,
+    title: "Perfume 1",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
+    url: "perfume_1.jpg",
+    qty: 0,
+  },
+  {
+    id: 7,
+    title: "Perfume 2",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
+    url: "perfume_2.jpg",
+    qty: 0,
+  },
+  {
+    id: 8,
+    title: "Perfume 3",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, alias?",
+    url: "perfume_3.jpg",
+    qty: 0,
+  },
+];
 
 function displayProduct() {
-    let innerDisplayProduct = ''
+  let innerDisplayProduct = "";
 
-    allProducts.forEach((element, index) => {
-        innerDisplayProduct += `<div class="shop-card">
+  allProducts.forEach((element, index) => {
+    innerDisplayProduct += `<div class="shop-card">
         <div class="imgBx">
             <img src="../images/${element.url}" alt="Men Product One" />
 
@@ -94,32 +93,37 @@ function displayProduct() {
             </div>
             <div class="rating">
                 <button type="button" class="btn btn-dark"
-                onclick="addToCart('${index}','${element.title}','${element.url}','${element.price}')"
+                onclick="addToCart('${element.id}','${element.title}','${element.url}','${element.price}')"
                 >
                     Add to cart
                 </button>
 
             </div>
         </div>
-    </div>`
-    })
+    </div>`;
+  });
 
-    let parentProductDiv = document.getElementById('parent-product-div')
-    parentProductDiv.innerHTML = innerDisplayProduct
+  let parentProductDiv = document.getElementById("parent-product-div");
+  parentProductDiv.innerHTML = innerDisplayProduct;
 }
 
+function addToCart(id, title, url, price) {
+  let getCartQ = localStorage.getItem("cartQ");
+  getCartQ= parseInt(getCartQ)
+  if (getCartQ) {
+    localStorage.setItem("cartQ", getCartQ+1)
+    console.log(getCartQ);
+  } else {
+    localStorage.setItem("cartQ", 1);
+  }
 
-function addToCart(id,title,url,price) {
-   let product={
-    id,
-    title,
-    url,
-    price
-   }
-   console.log(product)
+  //    let product={
+  //     id,
+  //     title,
+  //     url,
+  //     price
+  //    }
+  //    console.log(product)
 }
 
-
-
-
-displayProduct()
+displayProduct();
